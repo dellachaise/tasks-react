4f20d1861b63ea01f06b0377c199dccd57aaf6c8
Dasha <dellachaise@gmail.com>
2017-02-21 17:23:27 +0200

    Implemented simple react app

---
+++ src/components/list.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default function list (){
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+        </ul>
+    );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
+var path        = require('path'),
+  HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
 module.exports = {
-	entry: './src/app.js',
-	output: {
-		filename: 'bundle.js',
-		path: './dist'
-	}
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: './dist'
+    },
+    module : {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['react']
+          }
+        }
+      ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src', 'index.html')
+    })]
 };
