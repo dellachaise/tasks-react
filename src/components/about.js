153fec516dadd9b7f19e5f6c4554126be12c9b6c
Dasha <dellachaise@gmail.com>
2017-02-26 20:52:41 +0200

    Added loading icon

--- src/components/about.js
+++ src/components/about.js
@@ -7,26 +7,45 @@ export default class About extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            page: {}
+            page: {},
+            loading: true
         };
     }
 
     componentDidMount() {
-        fetch("http://127.0.0.1:8000/pages/about/")
+        fetch("http://127.0.0.1:8000/pages/about/?sleep=10")
             .then(response => {
                 return response.json();
             })
             .then(json => {
-                this.setState({ page: json });
+                this.setState({ 
+                    page: json,
+                    loading: false
+                 });
             })
+    }
+
+    renderLoading() {
+        return <div>Loading...</div>;
+    }
+
+    renderPosts() {
+        return (
+            <div>
+                <h1>{this.state.page.title}</h1>
+                <p>{this.state.page.content}</p>
+            </div>
+        )
     }
 
     render() {
         return (
             <div>
                 <Helmet title="About" />
-                <h1>{this.state.page.title}</h1>
-                {this.state.page.content}
+                {this.state.loading ?
+                    this.renderLoading()
+                    : this.renderPosts()
+                }
             </div>
         );
     }
